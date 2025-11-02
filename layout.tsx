b90755993e00f1343afbb786bbcb0df33a2e2614
ngalang290898-svg <ngalang290898@gmail.com>
2025-11-02 22:24:07 +0800

    Add files via upload

---
+++ layout.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import './globals.css';
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body className="flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-grow">{children}</main>
+        <Footer />
+      </body>
+    </html>
+  );
+}
---
+++ page.tsx
@@ -0,0 +1,11 @@
+import HeroSection from '@/components/HeroSection';
+import WelcomeSection from '@/components/WelcomeSection';
+
+export default function HomePage() {
+  return (
+    <>
+      <HeroSection />
+      <WelcomeSection />
+    </>
+  );
+}
